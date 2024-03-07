459d138af16ba35327719906831af55d67a68ed3
hungvq <hungvq@live.com>
2024-03-07 09:41:35 +0700

    break routing

---
+++ routing.module.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/app.guard';
+
+export const FINANCIALRoutes: Routes = [
+    
+    { path: 'general-ledger', loadChildren: () => import('./general-ledger/general-ledger.module').then(m => m.GeneralLedgerPageModule), canActivate: [AuthGuard] },
+    { path: 'general-ledger/:id', loadChildren: () => import('./general-ledger-detail/general-ledger-detail.module').then(m => m.GeneralLedgerDetailPageModule), canActivate: [AuthGuard] },
+    { path: 'tax-definition', loadChildren: () => import('./tax-definition/tax-definition.module').then(m => m.TaxDefinitionPageModule), canActivate: [AuthGuard] },
+    { path: 'tax-definition/:id', loadChildren: () => import('./tax-definition-detail/tax-definition-detail.module').then(m => m.TaxDefinitionDetailPageModule), canActivate: [AuthGuard] },
+  
+];
